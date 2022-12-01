774d08de022ea01f8e419364d3782a5b52bd3b00
ms747 <mayur.shah.ha@gmail.com>
2022-12-01 13:11:08 +0530

    Day 1

---
+++ day1/main.rs
@@ -0,0 +1,21 @@
+const INPUT: &str = include_str!("./input.txt");
+
+fn main() {
+    let mut data = INPUT
+        .split("\n\n")
+        .map(|x| x.trim().split('\n'))
+        .map(|x| x.fold(0, |acc, x| acc + x.parse::<u32>().unwrap()))
+        .collect::<Vec<u32>>();
+
+    // Sort
+    data.sort_by(|a, b| b.cmp(a));
+
+    // Part 1
+    let part1: u32 = data.iter().take(1).sum();
+
+    // Part 2
+    let part2: u32 = data.iter().take(3).sum();
+
+    println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
+}
