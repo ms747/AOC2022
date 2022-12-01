4d271927c916b33608c0cc5c9808c51876958c25
ms747 <mayur.shah.ha@gmail.com>
2022-12-01 13:39:37 +0530

    Day 1

--- day1/main.rs
+++ day1/main.rs
@@ -1,11 +1,11 @@
 const INPUT: &str = include_str!("./input.txt");
 
 fn main() {
-    let mut data = INPUT
+    let mut data: Vec<u32> = INPUT
         .split("\n\n")
         .map(|x| x.trim().split('\n'))
         .map(|x| x.fold(0, |acc, x| acc + x.parse::<u32>().unwrap()))
-        .collect::<Vec<u32>>();
+        .collect();
 
     // Sort
     data.sort_by(|a, b| b.cmp(a));
