a0380798b781c39d369f9137dd5e8e29b17524b0
ms747 <mayur.shah.ha@gmail.com>
2022-12-07 01:13:54 +0530

    Day 6

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,11 +1,14 @@
 use std::time::Instant;
 
-fn unique(chrs: &[u8], window_size: usize) -> bool {
-    for x in 0..window_size {
-        for y in 0..window_size {
-            if x != y && chrs[x] == chrs[y] {
-                return false;
-            }
+fn unique(chrs: &[u8]) -> bool {
+    let mut seen: u32 = 0;
+
+    for c in chrs {
+        let prev = seen;
+        seen |= 1 << (c - b'a');
+
+        if seen == prev {
+            return false;
         }
     }
 
@@ -14,7 +17,7 @@ fn unique(chrs: &[u8], window_size: usize) -> bool {
 
 fn solve(chrs: &[u8], window_size: usize) -> usize {
     for (idx, window) in chrs.windows(window_size).enumerate() {
-        let is_unique = unique(window, window_size);
+        let is_unique = unique(window);
         if is_unique {
             return idx + window_size;
         }
@@ -27,10 +30,10 @@ fn main() {
     let input = include_str!("input6.txt").as_bytes();
 
     let it = Instant::now();
-    let part1 = solve(&input, 4);
+    let part1 = solve(input, 4);
     println!("{part1} {:?}", it.elapsed());
 
     let it = Instant::now();
-    let part2 = solve(&input, 14);
+    let part2 = solve(input, 14);
     println!("{part2} {:?}", it.elapsed());
 }
